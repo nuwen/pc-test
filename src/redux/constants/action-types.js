8e961c17f6f493634129bf5aae149f8b3770e6e6
Danton Nguyen <dantonnguyen88@gmail.com>
2019-07-22 22:23:42 -0700

    begin redux implementation

---
+++ src/redux/constants/action-types.js
@@ -0,0 +1,1 @@
+export const ADD_VIEWED_ACCOUNT = "ADD_VIEWED_ACCOUNT";
---
+++ src/redux/store/index.js
@@ -0,0 +1,10 @@
+import {createStore,applyMiddleware, compose} from "redux"
+import reducer from "../reducers/index"
+import thunk from "redux-thunk";
+
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, storeEnhancers(applyMiddleware(thunk)));
+window.store = store;
+
+export default store;
