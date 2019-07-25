31f7efbabf8a2cdd749d5dea8b0ae8728da2bac6
Danton Nguyen <dantonnguyen88@gmail.com>
2019-07-24 22:13:52 -0700

    remove fetch user function

--- src/js/api.js
+++ src/js/api.js
@@ -1,19 +1,18 @@
 export async function fetchUserAccounts(){
   let url = "https://dev.presscentric.com/test/accounts"
   let response = await fetch(url)
-
   let data = await response.json();
   return data;
 }
 
 export async function deleteUserAccount(id){
-  // let url = "https://dev.presscentric.com/test/accounts/" + id
-  // let response = await fetch(url, {
-  //   method: "DELETE"
-  // })
+  let url = "https://dev.presscentric.com/test/accounts/" + id
+  let response = await fetch(url, {
+    method: "DELETE"
+  })
 
-  // let data = await response.json();
-  // return data;
+  let data = await response;
+  console.log( data);
 
   console.log(id + ' successfully deleted')
 }
