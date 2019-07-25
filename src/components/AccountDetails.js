a81f557d86626d4756388c1c3ac2a695f59d5dc3
Danton Nguyen <dantonnguyen88@gmail.com>
2019-07-24 20:12:02 -0700

    Update delete button based on clicked on button

--- src/components/AccountDetails.js
+++ src/components/AccountDetails.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const AccountDetails = ({selected: {id, name}}) => {
+
+function AccountDetails({selected: {id, name}}) {
+
+  let [btnCount, setBtnCount] = useState(0)
+  let [btnText, setBtnText] = useState('Delete')
+
+  function incrementCount(){
+    setBtnCount(btnCount => ++btnCount)
+  }
+  
+  if(btnCount === 1 && btnText !== 'Confirm Delete'){
+    setBtnText('Confirm Delete')
+  }
+
+  if(btnCount === 2){
+    setBtnCount(0)
+    setBtnText('Delete')
+  }
+  
 
   return(
     <div>
       <h2>Id: {id} - {name}</h2>
+      <p>{btnCount}</p>
+      <button onClick={incrementCount}>{btnText}</button>
     </div>
   )
 }
